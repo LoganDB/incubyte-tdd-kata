ee9d74cd2f561867dac169fc85db190f39630075
shaquib.afzal379 <shaquib.afzal379@gmail.com>
2025-04-15 20:59:38 +0530

    Test2: Single number returns itself (RED)

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,7 +1,9 @@
 import unittest
 from string_calculator import add
 
-# Empty string returns zero
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string_returns_zero(self):
         self.assertEqual(0,add(""))
+
+    def test_single_number_returns_its_value(self):
+        self.assertEqual(1,add("1"))
