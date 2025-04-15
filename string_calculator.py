6e09844f4565fd7d3588030dddc67efc573149ee
shaquib.afzal379 <shaquib.afzal379@gmail.com>
2025-04-15 21:24:38 +0530

    Added negative number check (GREEN)

--- string_calculator.py
+++ string_calculator.py
@@ -6,4 +6,8 @@ def add(numbers):
         delimiter=numbers[2]
         numbers=numbers[4:]
     nums=numbers.replace('\n',delimiter).split(delimiter)
-    return sum (int(num) for num in nums)
+    numbers_list=[int(num) for num in nums]
+    negatives=[num for num in numbers_list if num < 0]
+    if negatives:
+        raise ValueError(f"negative numbers not allowed {negatives[0]}")
+    return sum (numbers_list)
