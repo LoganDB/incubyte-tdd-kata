a006c0a569ac64678d2aca6c7d84c663c4aae60e
shaquib.afzal379 <shaquib.afzal379@gmail.com>
2025-04-15 21:04:03 +0530

    Test3: Two numbers comma delimited return sum (RED)

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -7,3 +7,6 @@ class TestStringCalculator(unittest.TestCase):
 
     def test_single_number_returns_its_value(self):
         self.assertEqual(1,add("1"))
+    
+    def test_two_numbers_comma_delimited_returns_sum(self):
+        self.assertEqual(3,add("1,2"))
